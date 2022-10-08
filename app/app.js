dad4fcde8f76855cca1cfa95818ac0e73c44667c
KSoft-Solution <kanhasahu955902@gmail.com>
2022-10-09 01:12:43 +0530

    static files added

--- app/app.js
+++ app/app.js
@@ -74,16 +74,12 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors());
-// deployment
-
-console.log(process.env.NODE_ENV)
 
 __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../../frontend/build')))
-
+    app.use(express.static(path.join(__dirname, 'build')))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../../frontend', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
     });
 } else {
     app.get('/', (req, res) => {
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,9 +2,6 @@ const router = require('express').Router();
 const apiRoutes = require('./api.routes')
 
 router.use(process.env.API_URL, apiRoutes);
-router.use('/', (req, res)=>{
-  res.status(200).json({message:`success-${req.originalUrl} Found`})
-});
 router.use(process.env.API_URL, (req, res) =>
   res.status(404).json({message:`Error-${req.originalUrl} Not Found`})
 );
