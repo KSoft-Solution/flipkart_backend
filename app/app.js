c335c2402e20b07a89379a7e9e4b4e27de52c8e6
KSoft-Solution <kanhasahu955902@gmail.com>
2022-10-09 19:03:45 +0530

    rebase

--- app/app.js
+++ app/app.js
@@ -76,7 +76,7 @@ app.use((req, res, next) => {
 app.use(cors());
 
 __dirname = path.resolve();
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'build')))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
