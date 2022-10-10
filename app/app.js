759625d2955bfd24f300208ec4f9dfe0aa7f76b2
KSoft-Solution <kanhasahu955902@gmail.com>
2022-10-11 01:45:04 +0530

    rebase

--- app/app.js
+++ app/app.js
@@ -34,7 +34,7 @@ app.use(compression());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET,PATCH,POST,DELETE,PUT");
   res.header("Access-Control-Expose-Headers", "Content-Length");
   res.header(
     "Access-Control-Allow-Headers",
